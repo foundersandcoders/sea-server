bfcb4244d34e93938bed7033398d3c60f06b9fa6
FilWisher <williamfisher@hotmail.co.uk>
2015-04-18 23:24:32 +0100

    add makefile and add hello.c

---
+++ Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-Wall -g
+
+clean:
+	rm -f hello
+
+all:
+	make hello
---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+
+    const char words[] = "Hello World";
+
+    puts(words);
+
+    return 0;
+}
